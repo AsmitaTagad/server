3732510a8491d5b06b191f7560ec50414ced9689
Ruturaj <ruturajmengal2016@gmail.com>
2023-05-14 20:11:16 +0530

    done

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
@@ -8,10 +15,30 @@ const io = new Server(server, {
   methods: ["GET", "POST", "PUT"],
 });
 
+const users = [{}];
+
 io.on("connection", (socket) => {
-  console.log("a user connected");
+  socket.on("join", (user) => {
+    users[socket.id] = user;
+    socket.broadcast.emit("Hello", {
+      user: users[socket.id],
+      message: "Joined",
+    });
+  });
+  socket.on("message", (data) => {
+    io.emit("send", {
+      user: users[data.id],
+      message: data.message,
+    });
+  });
+  socket.on("disconnect", () => {
+    io.emit("left", {
+      user: users[socket.id],
+      message: "I am leaving...",
+    });
+  });
 });
 
-server.listen(3000, () => {
-  console.log("listening on :3000");
+server.listen(5000, () => {
+  console.log("listening on 5000");
 });
